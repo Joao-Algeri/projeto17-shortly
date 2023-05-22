f26d1979d2cdacf5b6b020fbb00f1c5b69ec9446
JoaoAlgeri <joaoalgeri@gmail.com>
2023-05-22 08:09:14 -0300

    feat: sign in function added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/schemaValidator.js";
 import userSchema from "../schemas/userSchema.js";
 import { signUp } from "../controllers/users.controller.js";
-
+import { signIn } from "../controllers/auth.controller.js";
+import loginSchema from "../schemas/loginSchema.js";
 
 const authRouter = Router();
 
-
+authRouter.post("/signin", validateSchema(loginSchema), signIn);
 authRouter.post("/signup", validateSchema(userSchema), signUp);
 
 export default authRouter;
--- src/schemas/loginSchema.js
+++ src/schemas/loginSchema.js
@@ -1,21 +1,8 @@
-export async function createUser(req, res) {
-    const { name, email, password } = req.body
-  
-    try {
-  
-      const userExists = await db.query(`SELECT * FROM users WHERE email = $1`, [email])
-  
-      if (userExists.length > 0) return res.sendStatus(409)
-  
-      const hash = bcrypt.hashSync(password, 10)
-  
-      await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`, [name, email, hash])
-  
-      res.sendStatus(201)
-  
-    } catch (error) {
-      console.log(error)
-      res.status(500).send(error.message)
-    }
-  
-  }
+import joi from 'joi';
+
+const loginSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().required()
+});
+
+export default loginSchema;
