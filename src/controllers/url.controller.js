408d1c722a9c09bea3e51bd76f88f7abb1593414
JoaoAlgeri <joaoalgeri@gmail.com>
2023-05-22 10:15:51 -0300

    feat: get url by id function added

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -24,6 +24,28 @@ export async function urlShortener(req, res) {
     console.log(error)
     res.status(500).send('There is a problem in our servers, try again later')
   }
-
-
 }
+
+export async function getUrlById(req, res) {
+    const { id } = req.params
+  
+    try {
+
+      const result = await db.query(`SELECT * FROM shortens WHERE id = $1`, [id])
+  
+      if (result.rowCount === 0) return res.sendStatus(404)
+  
+      const [url] = result.rows
+  
+      res.send({
+        id: url.id,
+        shortUrl: url.shortUrl,
+        url: url.url
+      })
+  
+    } catch (error) {
+      console.log(error)
+      res.status(500).send("There is a problem in our servers, try again later")
+    }
+  
+  }
--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -2,11 +2,12 @@ import { urlsRouter } from "express";
 import { authValidation } from "../middlewares/authorization.middleware.js";
 import urlSchema from "../schemas/urlSchema.js";
 import { validateSchema } from "../middlewares/schemaValidator.js";
-import { urlShortener } from "../controllers/url.controller.js";
+import { getUrlById,urlShortener } from "../controllers/url.controller.js";
 
 const urlsRouter = Router();
 
 urlsRouter.post("/urls/shorten",validateSchema(urlSchema),authValidation,urlShortener);
+urlsRouter.get("/urls/:id", getUrlById);
 
 
 export default urlsRouter;
