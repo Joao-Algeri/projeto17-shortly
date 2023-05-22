382f2cc9147e1eec5b30f393f1f83338e00bcdb2
JoaoAlgeri <joaoalgeri@gmail.com>
2023-05-22 10:37:38 -0300

    feat:delete shortened url function added

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -49,3 +49,48 @@ export async function getUrlById(req, res) {
     }
   
   }
+
+  export async function openUrl(req, res) {
+
+    const { shortUrl } = req.params
+  
+    try {
+      const result = await db.query(`SELECT * FROM shortens WHERE "shortUrl" = $1`, [shortUrl])
+  
+      if (result.rowCount === 0) return res.sendStatus(404)
+  
+      const [url] = result.rows
+  
+      await db.query(`UPDATE shortens SET "visitCount" = "visitCount" + 1 WHERE id = $1`, [url.id])
+  
+      res.redirect(url.url)
+  
+    } catch (error) {
+      console.log(error)
+      res.status(500).send("There is a problem in our servers, try again later")
+    }
+  
+  }
+
+  export async function deleteUrl(req, res) {
+    const { id } = req.params
+    const { user } = res.locals
+  
+    try {
+      const result = await db.query(`SELECT * FROM shortens WHERE id = $1`, [id])
+  
+      if (result.rowCount === 0) return res.sendStatus(404)
+  
+      const [url] = result.rows
+  
+      if (url.userId !== user.id) return res.sendStatus(401)
+  
+      await db.query(`DELETE FROM shortens WHERE id = $1`, [id])
+  
+      res.sendStatus(204)
+      
+    } catch (error) {
+      console.log(error)
+      res.status(500).send("There is a problem in our servers, try again later")
+    }
+  }
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,10 @@
 import {Router} from "express";
 import authRouter from "./auth.routes.js";
+import urlsRouter from "./urls.routes.js";
 
 const router = Router();
 
+router.use(urlsRouter);
 router.use(authRouter);
-
 
 export default router;
