a0bb337e8d348f44d451af260309edac0b716ef6
JoaoAlgeri <joaoalgeri@gmail.com>
2023-05-22 10:29:37 -0300

    feat:open shorten url function added

--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -2,12 +2,13 @@ import { urlsRouter } from "express";
 import { authValidation } from "../middlewares/authorization.middleware.js";
 import urlSchema from "../schemas/urlSchema.js";
 import { validateSchema } from "../middlewares/schemaValidator.js";
-import { getUrlById,urlShortener } from "../controllers/url.controller.js";
+import { getUrlById, openUrl, urlShortener } from "../controllers/url.controller.js";
 
 const urlsRouter = Router();
 
 urlsRouter.post("/urls/shorten",validateSchema(urlSchema),authValidation,urlShortener);
 urlsRouter.get("/urls/:id", getUrlById);
+urlsRouter.get("/urls/open/:shortUrl", openUrl);
 
 
 export default urlsRouter;
