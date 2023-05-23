3a7edcf493f9cb2cba198f88262e7984f31ec9ab
JoaoAlgeri <joaoalgeri@gmail.com>
2023-05-23 15:29:39 -0300

    fixing bugs

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -4,7 +4,7 @@ const userSchema = joi.object({
   name: joi.string().required(),
   email: joi.string().required().email(),
   password: joi.string().required(),
-  confirmPassword: joi.ref("password")
+  confirmPassword: joi.string().required().valid(joi.ref("password"))
 });
 
 export default userSchema;
